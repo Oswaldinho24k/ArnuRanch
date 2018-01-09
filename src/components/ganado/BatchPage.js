20d85c5a5d4bd632c5387b34df0e4e301afb9bf2
Miguel Rafael González Durón <miglez90290995@gmail.com>
2018-01-09 13:42:35 -0600

    BatchPage Added

---
+++ src/components/ganado/BatchPage.js
@@ -0,0 +1,29 @@
+import React, {Component} from 'react';
+import {Table} from 'antd';
+
+class BatchPage extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {};
+    }
+
+    render() {
+        return (
+            <div>
+
+            </div>
+        );
+    }
+}
+
+const mapStateToProps = () => ({
+
+});
+
+const mapDispatchToProps = () => ({
+
+});
+
+BatchPage = connect(mapStateToProps,mapDispatchToProps)(BatchPage);
+export default BatchPage;
+
