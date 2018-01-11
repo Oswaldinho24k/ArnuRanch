9838a99750f6911ce16f0a514560cceac84e2775
Oswaldinho <oswalfut_96@hotmail.com>
2018-01-11 11:41:55 -0600

    :boom:

--- src/Api/Django.js
+++ src/Api/Django.js
@@ -9,6 +9,8 @@ let debug = true;
 let animalsUrl = 'http://localhost:8000/api/ganado/animals/';
 let tokenUrl = 'http://localhost:8000/api/auth/token-auth/';
 let userUrl = 'http://localhost:8000/api/auth/me/';
+let lotesUrl = 'http://localhost:8000/api/ganado/lotes/';
+let corralesUrl = 'http://localhost:8000/api/ganado/corrales/';
 
 
 
@@ -17,6 +19,8 @@ if(!debug){
     animalsUrl = 'https://arnu-ranch-backend.herokuapp.com/api/ganado/animals/';
     tokenUrl = 'https://arnu-ranch-backend.herokuapp.com/token-auth/';
     userUrl = 'https://arnu-ranch-backend.herokuapp.com/auth/me/';
+    lotesUrl = 'https://arnu-ranch-backend.herokuapp.com/api/ganado/lotes/';
+    corralesUrl = 'https://arnu-ranch-backend.herokuapp.com/api/ganado/corrales/';
 
 }
 
@@ -25,7 +29,10 @@ if(!debug){
 const api = {
     /*-----------------animals functions-----------------------*/
     //Get all animals
-    getAnimals:()=>{
+    getAnimals:(url)=>{
+
+        if(url)animalsUrl=url;
+
         const userToken = JSON.parse(localStorage.getItem('userRanchoToken'));
         return new Promise(function (resolve, reject) {
             const instance = axios.create({
@@ -73,9 +80,100 @@ const api = {
     /*-----------------aliments functions-----------------------*/
 
     /*-----------------lotes functions-----------------------*/
+    getLotes:()=>{
+        const userToken = JSON.parse(localStorage.getItem('userRanchoToken'));
+        return new Promise(function (resolve, reject) {
+            const instance = axios.create({
+                baseURL: lotesUrl,
+                // timeout: 2000,
+                headers: {
+                    'Content-Type': 'application/json',
+                    'Authorization': 'Token ' + userToken
+                }
+            });
+            instance.get('')
+                .then(function (response) {
+                    resolve(response.data);
+                })
+                .catch(function (error) {
+                    console.log('el error: ', error.response);
+                    reject(error);
+                });
+
+
+        });
+    },
+    newLote:(lote)=>{
+        const userToken = JSON.parse(localStorage.getItem('userRanchoToken'));
+        return new Promise(function (resolve, reject) {
+            const instance = axios.create({
+                baseURL: lotesUrl,
+                // timeout: 2000,
+                headers: {
+                    'Content-Type': 'application/json',
+                    'Authorization': 'Token ' + userToken
+                }
+            });
+            instance.post('', lote)
+                .then(function (response) {
+                    resolve(response.data);
+                })
+                .catch(function (error) {
+                    console.log('el error: ', error.response);
+                    reject(error);
+                });
+
+
+        });
+    },
 
     /*-----------------corrales functions-----------------------*/
+    getCorrales:()=>{
+        const userToken = JSON.parse(localStorage.getItem('userRanchoToken'));
+        return new Promise(function (resolve, reject) {
+            const instance = axios.create({
+                baseURL: corralesUrl,
+                // timeout: 2000,
+                headers: {
+                    'Content-Type': 'application/json',
+                    'Authorization': 'Token ' + userToken
+                }
+            });
+            instance.get('')
+                .then(function (response) {
+                    resolve(response.data);
+                })
+                .catch(function (error) {
+                    console.log('el error: ', error.response);
+                    reject(error);
+                });
 
+
+        });
+    },
+    newCorral:(corral)=>{
+        const userToken = JSON.parse(localStorage.getItem('userRanchoToken'));
+        return new Promise(function (resolve, reject) {
+            const instance = axios.create({
+                baseURL: lotesUrl,
+                // timeout: 2000,
+                headers: {
+                    'Content-Type': 'application/json',
+                    'Authorization': 'Token ' + userToken
+                }
+            });
+            instance.post('', corral)
+                .then(function (response) {
+                    resolve(response.data);
+                })
+                .catch(function (error) {
+                    console.log('el error: ', error.response);
+                    reject(error);
+                });
+
+
+        });
+    },
 
 
     /*-----------------user functions-----------------------*/
