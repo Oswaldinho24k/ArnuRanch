15732653518421477bf534ca604a2f830e636a20
Miguel Gonzalez <miglez90290995@gmail.com>
2018-02-02 01:25:16 -0600

    changing part two

--- src/components/plantaAlimentos/InsumosForm.js
+++ src/components/plantaAlimentos/InsumosForm.js
@@ -1,5 +1,5 @@
 import React from "react";
-import {Button, Form, Input, InputNumber} from 'antd';
+import {Button, Form, Input, InputNumber, Modal} from 'antd';
 const FormItem = Form.Item;
 
 //TODO create CRUD of this component
@@ -9,7 +9,7 @@ const style = {
     justifyContent:'space-around',
     flexWrap:'wrap'
 };
-let InsumosDisplay = ({form, onSubmit}) => {
+let InsumosDisplay = ({form, onSubmit, title, width, onCancel}) => {
     const handleSubmit = e => {
         e.preventDefault();
         form.validateFields((err, values) => {
@@ -22,56 +22,65 @@ let InsumosDisplay = ({form, onSubmit}) => {
     };
     const { getFieldDecorator } = form;
     return (
-        <Form style={style} onSubmit={handleSubmit}>
-            <FormItem label="Nombre">
-                {
-                    getFieldDecorator(
-                        'name',
-                        {
+        <Modal
+            title={title}
+            visible={true}
+            width={width}
+            maskClosable={true}
+            footer={[null, null]}
+            onCancel={onCancel}
+        >
+            <Form style={style} onSubmit={handleSubmit}>
+                <FormItem label="Nombre">
+                    {
+                        getFieldDecorator(
+                            'name',
+                            {
+                                rules: [{
+                                    required: true, message: 'Completa el campo!',
+                                }],
+                            }
+                        )(<Input/>)
+                    }
+                </FormItem>
+                <FormItem label="Precio unitario">
+                    {
+                        getFieldDecorator('unit_price', {
                             rules: [{
                                 required: true, message: 'Completa el campo!',
                             }],
-                        }
-                    )(<Input/>)
-                }
-            </FormItem>
-            <FormItem label="Precio unitario">
-                {
-                    getFieldDecorator('unit_price', {
-                        rules: [{
-                            required: true, message: 'Completa el campo!',
-                        }],
-                    })(<InputNumber width="100%" min={1} max={10000}/>)
-                }
-            </FormItem>
+                        })(<InputNumber width="100%" min={1} max={10000}/>)
+                    }
+                </FormItem>
 
-            <FormItem label="Unidad">
-                {
-                    getFieldDecorator('unit', {
-                        rules: [{
-                            required: true, message: 'Completa el campo!',
-                        }],
-                    })(<Input/>)
-                }
+                <FormItem label="Unidad">
+                    {
+                        getFieldDecorator('unit', {
+                            rules: [{
+                                required: true, message: 'Completa el campo!',
+                            }],
+                        })(<Input/>)
+                    }
 
-            </FormItem>
-            <FormItem>
-                <Button
-                    type="primary"
-                    htmlType="submit"
-                    size="large"
-                    style={{
-                        borderColor: '#72c6cd',
-                        backgroundColor: '#72c6cd',
-                        display: 'flex',
-                        justifyContent: 'center',
-                        margin: '0 auto',
-                        width: '100%'
-                    }}
-                > Guardar
-                </Button>
-            </FormItem>
-        </Form>
+                </FormItem>
+                <FormItem>
+                    <Button
+                        type="primary"
+                        htmlType="submit"
+                        size="large"
+                        style={{
+                            borderColor: '#72c6cd',
+                            backgroundColor: '#72c6cd',
+                            display: 'flex',
+                            justifyContent: 'center',
+                            margin: '0 auto',
+                            width: '100%'
+                        }}
+                    > Guardar
+                    </Button>
+                </FormItem>
+            </Form>
+        </Modal>
     );
 };
 
