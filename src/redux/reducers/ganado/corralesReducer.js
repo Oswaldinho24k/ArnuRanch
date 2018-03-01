1e31b963dded7c5244924b7a30218f1ef5ddb861
Oswaldinho <oswalfut_96@hotmail.com>
2018-03-01 11:52:23 -0600

    corral actualizado

--- src/redux/reducers/ganado/corralesReducer.js
+++ src/redux/reducers/ganado/corralesReducer.js
@@ -1,6 +1,6 @@
 import {combineReducers} from 'redux';
 import {GET_CORRALES_SUCCESS, SAVE_CORRAL_SUCCESS} from "../../actions/ganado/corralesActions";
-import {SAVE_LOTE_SUCCESS} from "../../actions/ganado/lotesActions";
+import {EDIT_LOTE_SUCCESS, SAVE_LOTE_SUCCESS} from "../../actions/ganado/lotesActions";
 
 
 
@@ -11,7 +11,7 @@ function list(state=[], action){
         case SAVE_CORRAL_SUCCESS:
             return [...state, action.corral];
         case SAVE_LOTE_SUCCESS:
-            console.log(action.batch);
+
             let corrales = state.filter(c=>{
                return c.id !== action.batch.corral.id;
             });
@@ -19,6 +19,14 @@ function list(state=[], action){
             let corral = Object.assign({}, action.batch.corral);
             corral['lotes']= action.batch;
             return [...corrales, corral];
+        case EDIT_LOTE_SUCCESS:
+            let corralesList = state.filter(c=>{
+                return c.id !== action.lote.corral.id;
+            });
+            let corralC = Object.assign({}, action.lote.corral);
+            corralC['lotes']= action.lote;
+            return [...corralesList, corralC];
+
 
         default:
             return state;
