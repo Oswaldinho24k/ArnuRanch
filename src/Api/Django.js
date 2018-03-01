ac8397f705cad3f8c0a5e218b4e2d8e5523dbdc9
Oswaldinho <oswalfut_96@hotmail.com>
2018-03-01 12:42:10 -0600

    changed some urls

--- src/Api/Django.js
+++ src/Api/Django.js
@@ -54,6 +54,8 @@ if(!debug){
     itemsUrl = 'https://rancho.fixter.org/api/planta_alimentos/items/';
     formulasUrl = 'https://rancho.fixter.org/api/planta_alimentos/formulas/';
     egresosUrl = 'https://rancho.fixter.org/api/egresos/egresos/';
+    empresasUrl = 'https://rancho.fixter.org/api/api/ingresos/empresas/';
+    vacunasUrl = 'https://rancho.fixter.org/api/vacunas/vacunas/';
 
     /******************************Heroku Urls********************************/
     /*animalsUrl = 'https://arnu-ranch-backend.herokuapp.com/api/ganado/animals/';
