58360c51412c387766935df64bbf5d9e1598d18b
JoseLAmador <josel.amador.s@gmail.com>
2018-03-16 21:47:28 -0600

    another detail

--- src/components/empresas/InventarioEmpresa.js
+++ src/components/empresas/InventarioEmpresa.js
@@ -1,9 +1,13 @@
 import React, {Component} from 'react';
-import {Card, Select, Divider, Row} from 'antd';
+import {Card, Select, Divider, Row, Tabs, Table, Badge, Dropdown} from 'antd';
 import {Link} from 'react-router-dom';
 import {connect} from 'react-redux';
 import {bindActionCreators} from 'redux';
 import MainLoader from "../common/Main Loader";
+import TablePageB from "../clientes/TablePageB";
+
+const TabPane = Tabs.TabPane;
+
 
 
 const gridStyle = {
@@ -19,6 +23,8 @@ const gridStyle = {
 class InventarioEmpresa extends Component{
     state={
         dataIn:[],
+        key:"0",
+        datos:[]
     };
 
     componentWillMount(){
@@ -26,13 +32,82 @@ class InventarioEmpresa extends Component{
             dataIn:this.props.empresa
         })
     }
+
+    callback=(key)=>{
+        console.log(key)
+        this.setState({key:key})
+    }
+
+
+
     render(){
-        let {pathname, empresa, fetched} = this.props;
-        console.log(empresa)
-        //console.log(empresa.line_comp);
-        //let lineas = this.props.empresa.map((p)=>{p.linea_comp});
-        //console.log(lineas)
+
+        let {pathname, empresa, fetched, bline} = this.props;
         if(!fetched)return(<MainLoader/>);
+        let datos = [empresa.line_comp[this.state.key]];
+        let almacenes = datos.map(a=> a.almacenes);
+        let info = almacenes[0]
+        let items = info.map(a=> a.items);
+        console.log(items[0])
+        console.log(info)
+        console.log(empresa.line_comp !==undefined)
+
+        const expandedRowRender = () => {
+            const columns = [
+                {title: 'Item', dataIndex: 'id', key: 'id'},
+                {title: 'Tipo', dataIndex: 'product_type', key: 'product_type'},
+                {title: 'Cantidad', dataIndex: 'cantidad', key: 'cantidad'},
+                {title: 'Costo Unitario', dataIndex: 'costo_u', key: 'costo_u'},
+                {title: 'Total', dataIndex: 'total', key: 'total'},
+                {
+                    title: 'Action',
+                    dataIndex: 'operation',
+                    key: 'operation',
+                    render: () => (
+                        <span className="table-operation">
+            <a href="#">Pause</a>
+            <a href="#">Stop</a>
+
+          </span>
+                    ),
+                },
+            ];
+
+            console.log(info.items)
+            return (
+                <Table
+                    columns={columns}
+                    dataSource={items[0]}
+                    pagination={false}
+                />
+            );
+        }
+
+
+
+        const columns = [
+            {
+                title: 'Nombre de almacen',
+                dataIndex: 'name',
+                //render: (value) => console.log(value.name)
+
+            },
+            {
+                title: 'Items',
+                dataIndex: 'items',
+                render: items => items.length
+            },
+
+        ];
+
+
+
+
+        console.log(datos)
+        //console.log(empresa)
+        console.log(this.state.key)
+        //console.log(empresa.line_comp[this.state.key].almacenes)
+
         return(
             <div>
                 <div style={{marginBottom:10, color:'rgba(0, 0, 0, 0.65)' }}>
@@ -44,35 +119,31 @@ class InventarioEmpresa extends Component{
 
                 </div>
 
-                <Card title="Lineas de Negocio">
+                <Card>
+                    <h3>Lineas de Negocio</h3>
+                    <Tabs defaultActiveKey={this.state.key} onChange={this.callback}>
+                        {empresa.line_comp.length>0 ?
+                            empresa.line_comp.map((p, index) => {
+                                return(
+                                    <TabPane tab={p.name} key={index} />
+                                    )
+                            })
+                            :
+                            "Esta empresa no tiene lineas de negocio..."
+                            }
+                    </Tabs>
 
 
-                    {empresa.line_comp.length>0 ?
-                        empresa.line_comp.map(p =>(
-                            <Link to={`${pathname}/${p.id}`} style={{color:'black'}} key={p.id} >
-                                <Card.Grid style={gridStyle}>{p.name}</Card.Grid>
-                            </Link>
-                        ))
 
-                    :
-                    "Esta empresa no tiene lineas de negocio..."
-                    }
+                    <Table
+                        columns={columns}
+                        expandedRowRender={expandedRowRender}
+                        dataSource={info}
+                    />
 
 
 
-                    {/*<Link to={`/admin/empresas/inventario/almacen`} style={{color:'black'}} >
-                            <Card.Grid style={gridStyle}>Cerdos</Card.Grid>
-                        </Link>
-                        <Link to={`/admin/empresas/inventario/almacen`} style={{color:'black'}} >
-                            <Card.Grid style={gridStyle}>Ganado</Card.Grid>
-                        </Link>
-                        <Link to={`/admin/empresas/inventario/almacen`} style={{color:'black'}} >
-                            <Card.Grid style={gridStyle}>Granos</Card.Grid>
-                        </Link>
-                        <Link to={`/admin/empresas/inventario/almacen`} style={{color:'black'}} >
-                            <Card.Grid style={gridStyle}>Planta de Alimentos</Card.Grid>
-                        </Link>
-*/}
+
 
                 </Card>
 
@@ -83,17 +154,24 @@ class InventarioEmpresa extends Component{
 }
 
 function mapStateToProps(state, ownProps) {
+    console.log(ownProps)
     let pathname = ownProps.location.pathname;
-    let id = ownProps.match.params.em;
+    //let id = ownProps.match.params.em;
     let empresa = state.empresas.list.filter(a=>{
-        return id == a.id;
+        return pathname.slice(-1) == a.id;
     });
     empresa = empresa[0];
+    console.log(empresa)
+    //let bline = empresa.line_comp.map(b=>b)
+    //console.log(bline)
+
+
 
     return {
         pathname,
         empresa,
-        fetched: empresa!==undefined && state.empresas.list!==undefined,
+//        bline,
+        fetched: empresa!==undefined && state.empresas.list!==undefined && empresa.line_comp !==undefined,
     }
 }
 
@@ -105,3 +183,4 @@ function mapDispatchToProps(dispatch) {
 
 InventarioEmpresa = connect(mapStateToProps, mapDispatchToProps ) (InventarioEmpresa);
 export default InventarioEmpresa;
+
